d31bfe6ca6310a17c681ccaa134d21613a145250
dengconglin <dengconglin@thunisoft.com>
2021-04-13 11:10:35 +0800

    todo:堆排

--- src/sort/ArraySort.java
+++ src/sort/ArraySort.java
@@ -9,4 +9,31 @@ public class ArraySort {
             }
         }
     }
+
+
+    //region head sort region
+    public static void heapSort(int[] array) {
+        int length = array.length;
+        buildHeap(array,length);
+
+    }
+
+    private static void buildHeap(int[] array, int length) {
+        for (int i = length-1; i >0 ; i--) {
+
+        }
+    }
+
+    private static void doBuildHeap(int[] array,int i){
+
+    }
+
+    private static void swap(int[] array, int i , int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+
+    //endregion
 }
