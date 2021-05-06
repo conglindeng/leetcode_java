c75c710e5fe5301b8d5da1e4525b6f9ea3cdbdec
dengconglin <dengconglin@thunisoft.com>
2021-05-06 18:09:17 +0800

    1. some algorithm exercise

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 
+import algorithm.DeleteDuplicateNum;
+import algorithm.FindMaxKNums;
+import algorithm.MaxSubarraySum;
 import algorithm.slidingwindow.MaxSum;
-import leetcode.middle.CombinationSum4_377;
-import leetcode.middle.NumDecodings_91;
-import leetcode.middle.ShortestSeq_17_18;
+import leetcode.middle.*;
 import sort.ArraySort;
 
 import java.util.*;
@@ -15,10 +16,11 @@ public class Main {
         System.out.println(UUID.randomUUID().toString().replace("-", ""));
         int[] test1=new int[]{7,5,9,0,2,1,3,5,7,9,1,1,5,8,8,9,7};
         int[] test2=new int[]{1,5,9};
-       // new ShortestSeq_17_18().shortestSeq_new(test1,test2);
-       // MaxSum.maxSum_new(new int[]{1,3,-1,-3,5,3,6,7},3);
-        CombinationSum4_377.combinationSum4(new int[]{1,2,3},4);
-
+        LinkedHashMap<String,String> test=new LinkedHashMap<>();
+        test.put("1","1");
+        test.put("2","2");
+        test.put("3","3");
+        System.out.println(new MaxSubarraySum().MaxSubarraySum(new int[]{1, 4, -5, 9, 8, 3, -6}));
 
     }
 }
