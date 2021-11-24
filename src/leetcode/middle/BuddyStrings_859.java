2d0f1aeedb606f86f86f681d42713d4a35854641
dengconglin <dengconglin@thunisoft.com>
2021-11-24 11:33:32 +0800

    1. leetcode excersize

--- src/leetcode/middle/BuddyStrings_859.java
+++ src/leetcode/middle/BuddyStrings_859.java
@@ -42,4 +42,39 @@ public class BuddyStrings_859 {
         //size=2的话，则已经符合要求，否则s和goal相等，则判断s中是否有字母出现2次及以上
         return size == 2 || atLeastTwoTimes;
     }
+
+    public boolean buddyStrings_Optimize(String s, String goal) {
+        if (s == null || s.length() == 0 || goal == null || goal.length() == 0) {
+            return false;
+        }
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        //如果两个字符串相等，则需要判定是否至少有一个字母出现了2次以上
+        if (s.equals(goal)) {
+            int[] info = new int[26];
+            for (int i = 0; i < s.length(); i++) {
+                int index = s.charAt(i) - 'a';
+                if (info[index] > 0) {
+                    return true;
+                } else {
+                    info[index] += 1;
+                }
+            }
+        } else {
+            List<Integer> notEqualIndex = new ArrayList<>();
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) != goal.charAt(i)) {
+                    notEqualIndex.add(i);
+                }
+            }
+            if(notEqualIndex.size()!=2){
+                return false;
+            }
+            int index0=notEqualIndex.get(0);
+            int index1=notEqualIndex.get(1);
+            return s.charAt(index1)==goal.charAt(index0)&&s.charAt(index0)==goal.charAt(index1);
+        }
+        return false;
+    }
 }
