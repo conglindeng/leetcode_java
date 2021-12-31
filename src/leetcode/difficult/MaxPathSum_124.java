229f715e03d6b68c2f6127bb0b813668485442bf
dengconglin <dengconglin@thunisoft.com>
2021-12-31 18:15:33 +0800

    1. leetcode excersize

---
+++ src/leetcode/difficult/MaxPathSum_124.java
@@ -0,0 +1,30 @@
+package leetcode.difficult;
+
+import struct.TreeNode;
+
+public class MaxPathSum_124 {
+    int res = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        doGetMaxSum(root);
+        return res;
+    }
+
+    private int doGetMaxSum(TreeNode root) {
+        int left = 0;
+        if (root.left != null) {
+            left = doGetMaxSum(root.left);
+        }
+        int right = 0;
+        if (root.right != null) {
+            left = doGetMaxSum(root.right);
+        }
+        int cur = root.val + Math.max(left, right);
+        res = Math.max(res, cur);
+        if (cur < 0) {
+            return 0;
+        }
+        res = Math.max(left + right + root.val, res);
+        return cur;
+    }
+}
---
+++ src/leetcode/middle/MyPow_50.java
@@ -0,0 +1,7 @@
+package leetcode.middle;
+
+public class MyPow_50 {
+    public double myPow(double x, int n) {
+        return 0d;
+    }
+}
