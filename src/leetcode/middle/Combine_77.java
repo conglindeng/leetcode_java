b976125f531afecb63b521586222ed3bced99241
dengconglin <dengconglin@thunisoft.com>
2022-01-10 16:04:35 +0800

    1. leetcode excersize

--- src/leetcode/middle/Combine_77.java
+++ src/leetcode/middle/Combine_77.java
@@ -4,12 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Combine_77 {
-    public List<List<Integer>> combine(int n, int k) {
+    List<List<Integer>> res = new ArrayList<>();
 
-        return null;
+    public List<List<Integer>> combine(int n, int k) {
+        List<Integer> cur = new ArrayList<>();
+        doCombine(cur, 1, n, k);
+        return res;
     }
 
-    private void doCombine(int n, int k) {
-
+    private void doCombine(List<Integer> cur, int idx, int n, int length) {
+        if (cur.size() == length) {
+            res.add(new ArrayList<>(cur));
+            return;
+        }
+        for (int i = idx; i <= n; i++) {
+            cur.add(i);
+            doCombine(cur, i + 1, n, length);
+            cur.remove(cur.size() - 1);
+        }
     }
 }
