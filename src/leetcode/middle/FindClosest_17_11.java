00e665888e21ef18840cc08ed28504ae2f1eac43
dengcl <1985144542@qq.com>
2022-05-27 18:32:19 +0800

    1.leetcode excercise

---
+++ src/leetcode/middle/FindClosest_17_11.java
@@ -0,0 +1,29 @@
+package leetcode.middle;
+
+/**
+ * @ClassName FindClosest_17_11
+ * @Description TODO
+ * @Author conglindeng
+ * @Date 2022/5/27 09:31
+ * @Version 1.0
+ */
+public class FindClosest_17_11 {
+
+    public int findClosest(String[] words, String word1, String word2) {
+        int index1 = -1;
+        int index2 = -1;
+        int res = Integer.MAX_VALUE;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(word1)) {
+                index1 = i;
+            } else if (words[i].equals(word2)) {
+                index2 = i;
+            }
+            if (index1 >= 0 && index2 >= 0) {
+                res = Math.min(res, Math.abs(index1 - index2));
+            }
+        }
+        return res;
+
+    }
+}
